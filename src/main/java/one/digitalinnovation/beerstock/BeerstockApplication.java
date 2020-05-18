6b5d6747bd16492763cfb66e80e5c3a282aeeb50
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-05-18 09:07:00 -0300

    Project created

---
+++ src/main/java/one/digitalinnovation/beerstock/BeerstockApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
